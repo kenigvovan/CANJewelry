97ce84b923930e155bb8f9bad64843d048054ab9
kenigvovan <koenigvovan@gmail.com>
2023-03-25 14:16:51 +0100

    values from canmods config

--- mods/canjewelry/src/Config.cs
+++ mods/canjewelry/src/Config.cs
@@ -39,17 +39,6 @@ namespace canjewelry.src
                 this.Comment += "]" + postfix;
             }
         }
-
-        //ECONOMY
-        public Part<double> EXP_CHISEL_USE = new Part<double>(5);
-        public Part<double> EXP_CHOPPED_LOG = new Part<double>(0.1);
-        public Part<double> EXP_FORGED_TOOL = new Part<double>(18);
-        public Part<double> EXP_FROM_KILLED_PLAYER_MIN_LAST_REVIVE_TOTAL_HOURS = new Part<double>(24);
-        public Part<bool> DROP_PLAYER_ARMOR = new Part<bool>(true);
-        public Part<bool> CHANCE_NOT_TO_DROP_ARMOR_PLAYER = new Part<bool>(false);
-        public Part<int> CHANCE_NOT_TO_DROP = new Part<int>(35);
-
-
         public Part<float> grindTimeOneTick = new Part<float>(3);
     }
 }
